353201635a9441ae94b7a23bc847eac844bb4c28
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-02 21:26:37 -0400

    setting up webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+import Icon from './images/pizza.jpeg'
+
+const element = document.createElement('div');
+
+function addImage() {
+const myIcon = new Image();
+    myIcon.src = Icon;
+    myIcon.classList.add('imageHome');
+
+    element.appendChild(myIcon);
+return element
+}
+
+addImage()
+
+console.log('hello')
